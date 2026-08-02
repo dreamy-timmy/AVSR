79b52006151035c61ef2b21804cfb35d80397f73
Tim Gunther <guntertimofej4@gmail.com>
2026-08-02 23:21:58 +0600

    added yt urls

---
+++ youtube_urls.py
@@ -0,0 +1,47 @@
+
+URLS = [
+        # RU
+        "https://www.youtube.com/watch?v=6hwENpQqKP0&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv", 
+        "https://www.youtube.com/watch?v=JlL-Tno5zvA&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=2",
+        "https://www.youtube.com/watch?v=sHtEOtwxvKQ&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=3",
+        "https://www.youtube.com/watch?v=GUHjkiFxuxo&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=4",
+        "https://www.youtube.com/watch?v=u_B36oDPtb4&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=5",
+        "https://www.youtube.com/watch?v=ZmwdHAhVsPM&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=6",
+        "https://www.youtube.com/watch?v=WTjfi-eqL7E&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=7",
+        "https://www.youtube.com/watch?v=aUTEZComjUw&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=8",
+        "https://www.youtube.com/watch?v=Y09Pfs2Ylx8&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=9",
+        "https://www.youtube.com/watch?v=aL145agf47s&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=10",
+        "https://www.youtube.com/watch?v=4kYSQmJ8pMM&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=11",
+        "https://www.youtube.com/watch?v=HNSjOIGiUxQ&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=12",
+        "https://www.youtube.com/watch?v=m7i30nUCyQk&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=13",
+        "https://www.youtube.com/watch?v=UzfAt6DoN3E&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=14",
+        "https://www.youtube.com/watch?v=Nx2GqsolBQ0&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=15",
+        "https://www.youtube.com/watch?v=SoinJsTXY9s&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=16",
+        "https://www.youtube.com/watch?v=WBHc4dXG1gE&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=17",
+        "https://www.youtube.com/watch?v=L1Qn2LEweeI&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=18",
+        "https://www.youtube.com/watch?v=nXUEw07DWkw&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=19",
+        "https://www.youtube.com/watch?v=o44ojhf7FoU&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=20",
+        "https://www.youtube.com/watch?v=mxP1Ig2Ejvw&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=21",
+        "https://www.youtube.com/watch?v=V7bU_F2_bfQ&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=22",
+        "https://www.youtube.com/watch?v=CJsptz95XYw&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=23",
+        "https://www.youtube.com/watch?v=LQZa7vPkqqc&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=24",
+        "https://www.youtube.com/watch?v=OSDIdNtlTzA&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=25",
+        "https://www.youtube.com/watch?v=DJN8jCmEYdE&list=PL3BJnp-dNqazNc11qgguXNcJwCMqwK5Yv&index=28",
+
+        "https://www.youtube.com/watch?v=PpTVc4eOXsw&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=2",
+        "https://www.youtube.com/watch?v=-aUus7YGnxg&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=3",
+        "https://www.youtube.com/watch?v=kR9w0pDwQsk&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=4",
+        "https://www.youtube.com/watch?v=uDTKcVPoTMc&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=5",
+        "https://www.youtube.com/watch?v=LV4zPrOJodY&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=6",
+        "https://www.youtube.com/watch?v=s_ad5zJJSiY&list=PLlx2izuC9gjgEWu2364R6GnrY8SMUYi-E&index=7",
+
+
+        # EN
+        "https://www.youtube.com/watch?v=5MuIMqhT8DM",
+        "https://youtu.be/MdZAMSyn_As?si=WOBfj6Qyas5PmbIF",
+        "https://youtu.be/iueVZJVEmEs?si=Yecra77TXcRof0jr",
+        "https://youtu.be/iNyUmbmQQZg?si=PJvLW0YdAd8wuEsB",
+        "https://youtu.be/qYvXk_bqlBk?si=YrACHe2kDx-nagp9",
+        "https://youtu.be/arj7oStGLkU?si=M8AckJ8G3NiAKKqS",
+        "https://youtu.be/5J6jAC6XxAI?si=bohcxaeXcWiYfP9n"
+    ]
